68deec11d52da09fe887e95995b7cfeeb535c16b
Murat Dogan <doganmurat@gmail.com>
2020-11-23 15:08:44 +0300

    test arm

--- cmake/toolchain/armv7.cmake
+++ cmake/toolchain/armv7.cmake
@@ -12,3 +12,6 @@ set(CMAKE_SYSROOT "$ENV{ARM_SYSROOT}")
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(CMAKE_C_FLAGS "-march=armv7-a -mfpu=neon-vfpv4 -mfloat-abi=hard -Wno-error=format-truncation" CACHE STRING "Compiler Flags")
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "Compiler Flags")
