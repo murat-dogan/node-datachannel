c4ab2c12f8622f3901cded1882649817b20e9798
Murat Dogan <doganmurat@gmail.com>
2020-11-23 15:37:20 +0300

    compile args

--- cmake/toolchain/armv7.cmake
+++ cmake/toolchain/armv7.cmake
@@ -13,5 +13,5 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-set(CMAKE_C_FLAGS "-march=armv7-a -mfpu=neon-vfpv4 -mfloat-abi=hard -Wno-error=format-truncation" CACHE STRING "Compiler Flags")
+set(CMAKE_C_FLAGS "-march=armv7-a -mfpu=neon-vfpv4 -mfloat-abi=hard -fPIC -Wno-error=format-truncation" CACHE STRING "Compiler Flags")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "Compiler Flags")
