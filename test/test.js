1f59f659e72eb2e3e113da391ab4c1d4d3983a58
Tim Perry <pimterry@gmail.com>
2022-03-07 20:48:28 +0100

    Add DataChannelStream to turn a data channel into a Node stream

--- test/test.js
+++ test/test.js
@@ -146,3 +146,47 @@ describe('P2P', () => {
 
     });
 });
+
+function waitForGathering(peer) {
+    return new Promise((resolve) => {
+        peer.onGatheringStateChange((state) => {
+            if (state === 'complete') resolve();
+        });
+        // Handle race conditions where gathering has already completed
+        if (peer.gatheringState() === 'complete') resolve();
+    });
+}
+
+describe('DataChannel streams', () => {
+
+    test('can build an echo pipeline', async () => {
+        let clientPeer = new nodeDataChannel.PeerConnection("Client", { iceServers: [] });
+        let echoPeer = new nodeDataChannel.PeerConnection("Client", { iceServers: [] });
+
+        const echoStream = new nodeDataChannel.DataChannelStream(
+            echoPeer.createDataChannel("echo-channel")
+        );
+        echoStream.pipe(echoStream); // Echo all received data back to the client
+
+        await waitForGathering(echoPeer);
+
+        const { sdp: echoDescSdp, type: echoDescType } = echoPeer.localDescription();
+        clientPeer.setRemoteDescription(echoDescSdp, echoDescType);
+        await waitForGathering(clientPeer);
+
+        const { sdp: clientDescSdp, type: clientDescType } = clientPeer.localDescription();
+        echoPeer.setRemoteDescription(clientDescSdp, clientDescType);
+
+        const clientChannel = await new Promise((resolve) => clientPeer.onDataChannel(resolve));
+
+        const clientResponsePromise = new Promise((resolve) => clientChannel.onMessage(resolve));
+        clientChannel.sendMessage("test message");
+
+        expect(await clientResponsePromise).toBe("test message");
+
+        clientChannel.close();
+        echoStream.destroy();
+        clientPeer.close();
+        echoPeer.close();
+    });
+});
