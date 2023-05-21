bdf39c69e33888e71c88c822372ddc33b40fc5ff
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2023-05-21 12:56:47 +0200

    fix: types license

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -237,6 +237,11 @@ export class DataChannelStream extends stream.Duplex {
 
 type BinaryType = 'arraybuffer' | 'blob';
 
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+
 interface RTCDataChannelEventMap {
     bufferedamountlow: Event;
     close: Event;
