4a0299e766bd99b1af6d3ce6b62619db0ef6b25d
Murat Dogan <doganmurat@gmail.com>
2023-08-08 22:59:54 +0300

    re-structure

--- polyfill/RTCIceCandidate.js
+++ polyfill/RTCIceCandidate.js
@@ -1,144 +1,107 @@
-// @ts-check
+// https://developer.mozilla.org/docs/Web/API/RTCIceCandidate
+//
+// Example: candidate:123456 1 UDP 123456 192.168.1.1 12345 typ host raddr=10.0.0.1 rport=54321 generation 0
 
-const componentMap = {
-    1: 'rtp',
-    2: 'rtcp',
-};
-
-/**
- * @class
- * @implements {RTCIceCandidate}
- */
-export default class {
-    /**
-     * @param  {RTCIceCandidateInit} init={}
-     */
-    constructor({ candidate, sdpMLineIndex, sdpMid, usernameFragment } = {}) {
+export default class _RTCIceCandidate {
+    constructor({ candidate, sdpMLineIndex, sdpMid, usernameFragment }) {
         if (sdpMLineIndex == null && sdpMid == null) {
-            throw new TypeError("Failed to construct 'RTCIceCandidate': sdpMid and sdpMLineIndex are both null.");
+            throw new Error('Failed to construct "RTCIceCandidate": sdpMid and sdpMLineIndex are both null.');
         }
-        this.#candidate = candidate;
-        this.#sdpMLineIndex = sdpMLineIndex ?? null;
-        this.#sdpMid = sdpMid ?? null;
-        this.#usernameFragment = usernameFragment ?? null;
+        this.candidate = candidate;
+        this.sdpMLineIndex = sdpMLineIndex ?? null;
+        this.sdpMid = sdpMid ?? null;
+        this.usernameFragment = usernameFragment ?? null;
 
-        if (candidate && candidate.indexOf('candidate:') !== -1) {
-            const interest = candidate.slice(candidate.indexOf('candidate:') + 10);
+        if (candidate) {
+            const fields = candidate.split(' ');
+            this.foundation = fields[0];
+            this.component = fields[1];
+            this.protocol = fields[2];
+            this.priority = parseInt(fields[3], 10);
+            this.ip = fields[4];
+            this.port = parseInt(fields[5], 10);
+            this.type = fields[7];
+            if (fields[6] === 'typ') {
+                this.tcpType = null;
+            } else if (fields[6] === 'tcp') {
+                this.tcpType = fields[7];
+                this.type = fields[8];
+            }
 
-            /** @type {any[]} split */
-            const [foundation, componentID, protocol, priority, ip, port, type, ...rest] = interest.split(' ');
-
-            this.#foundation = foundation;
-            this.#component = componentMap[componentID];
-
-            this.#protocol = protocol;
-            this.#priority = Number(priority);
-            this.#address = ip;
-            this.#port = Number(port);
-            this.#type = type;
-
-            if (type !== 'host') {
-                const raddrIndex = rest.indexOf('raddr');
-                if (raddrIndex !== -1) this.#relatedAddress = rest[raddrIndex + 1];
-
-                const rportIndex = rest.indexOf('rport');
-                if (rportIndex !== -1) this.#relatedPort = Number(rest[rportIndex + 1]);
+            // Parse the candidate string to extract relatedPort and relatedAddress
+            for (let i = 9; i < fields.length; i++) {
+                const field = fields[i];
+                if (field.startsWith('raddr')) {
+                    this.relatedAddress = field.split('=')[1];
+                } else if (field.startsWith('rport')) {
+                    this.relatedPort = parseInt(field.split('=')[1], 10);
+                }
             }
         }
     }
 
-    #address;
-
     get address() {
-        return this.#address ?? null;
+        return this.address || null;
     }
-
-    #candidate;
 
     get candidate() {
-        return this.#candidate || '';
+        return this.candidate || '';
     }
-
-    #component = null;
 
     get component() {
-        return this.#component;
+        return this.component;
     }
-
-    #foundation;
 
     get foundation() {
-        return this.#foundation ?? null;
+        return this.foundation || null;
     }
-
-    #port;
 
     get port() {
-        return this.#port ?? null;
+        return this.port || null;
     }
-
-    #priority;
 
     get priority() {
-        return this.#priority ?? null;
+        return this.priority || null;
     }
-
-    /** @type {RTCIceProtocol} */
-    #protocol;
 
     get protocol() {
-        return this.#protocol ?? null;
+        return this.protocol || null;
     }
-
-    #relatedAddress = null;
 
     get relatedAddress() {
-        return this.#relatedAddress;
+        return this.relatedAddress;
     }
-
-    #relatedPort;
 
     get relatedPort() {
-        return this.#relatedPort ?? null;
+        return this.relatedPort || null;
     }
-
-    #sdpMLineIndex;
 
     get sdpMLineIndex() {
-        return this.#sdpMLineIndex;
+        return this.sdpMLineIndex;
     }
-
-    #sdpMid;
 
     get sdpMid() {
-        return this.#sdpMid;
+        return this.sdpMid;
     }
-
-    #tcpType = null;
 
     get tcpType() {
-        return this.#tcpType;
+        return this.tcpType;
     }
-
-    /** @type {RTCIceCandidateType} */
-    #type;
 
     get type() {
-        return this.#type ?? null;
+        return this.type || null;
     }
 
-    #usernameFragment;
-
     get usernameFragment() {
-        return this.#usernameFragment;
+        return this.usernameFragment;
     }
 
     toJSON() {
         return {
-            candidate: this.#candidate,
-            sdpMLineIndex: this.#sdpMLineIndex,
-            sdpMid: this.#sdpMid,
-            usernameFragment: this.#usernameFragment,
+            candidate: this.candidate,
+            sdpMLineIndex: this.sdpMLineIndex,
+            sdpMid: this.sdpMid,
+            usernameFragment: this.usernameFragment,
         };
     }
 }
