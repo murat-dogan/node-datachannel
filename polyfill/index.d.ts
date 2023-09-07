227507bdf67d29c2241db3da87c4fb258e0cc8f0
Murat Dogan <doganmurat@gmail.com>
2023-09-08 00:31:46 +0200

    Export MessageEvent

--- polyfill/index.d.ts
+++ polyfill/index.d.ts
@@ -2,7 +2,7 @@ import RTCPeerConnection from './RTCPeerConnection.js';
 import RTCSessionDescription from './RTCSessionDescription.js';
 import RTCIceCandidate from './RTCIceCandidate.js';
 import RTCDataChannel from './RTCDataChannel.js';
-import { RTCDataChannelEvent, RTCPeerConnectionIceEvent } from './Events.js';
+import { RTCDataChannelEvent, RTCPeerConnectionIceEvent, MessageEvent } from './Events.js';
 
 export {
     RTCPeerConnection,
@@ -11,4 +11,5 @@ export {
     RTCDataChannel,
     RTCDataChannelEvent,
     RTCPeerConnectionIceEvent,
+    MessageEvent,
 };
--- polyfill/index.js
+++ polyfill/index.js
@@ -2,7 +2,7 @@ import RTCPeerConnection from './RTCPeerConnection.js';
 import RTCSessionDescription from './RTCSessionDescription.js';
 import RTCIceCandidate from './RTCIceCandidate.js';
 import RTCDataChannel from './RTCDataChannel.js';
-import { RTCDataChannelEvent, RTCPeerConnectionIceEvent } from './Events.js';
+import { RTCDataChannelEvent, RTCPeerConnectionIceEvent, MessageEvent } from './Events.js';
 
 export default {
     RTCPeerConnection,
@@ -11,4 +11,5 @@ export default {
     RTCDataChannel,
     RTCDataChannelEvent,
     RTCPeerConnectionIceEvent,
+    MessageEvent,
 };
