1afb0c8932e194c36772dd0b175defbb144f36a1
Murat Dogan <doganmurat@gmail.com>
2023-09-15 17:04:27 +0200

    try fPIC

--- cmake/toolchain/arm64.cmake
+++ cmake/toolchain/arm64.cmake
@@ -13,5 +13,6 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-set(CMAKE_C_FLAGS "-march=armv8-a -fPIC -Wno-error=format-truncation" CACHE STRING "Compiler Flags")
+# remove -fPIC
+set(CMAKE_C_FLAGS "-march=armv8-a -Wno-error=format-truncation" CACHE STRING "Compiler Flags")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "Compiler Flags")
