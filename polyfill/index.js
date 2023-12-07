7d2d7d2c1512f2fc21d6d1533e896c9c06ded39c
achingbrain <alex@achingbrain.net>
2023-12-07 18:18:38 +0000

    fix: export missing polyfills
    I need to use `RTCSessionDescription` but it's not exported so
    this PR Exports all implemented polyfills.
    
    It exports them as both named exports and properties of the default
    export for backwards compatibility.

--- polyfill/index.js
+++ polyfill/index.js
@@ -1,14 +1,23 @@
-import RTCPeerConnection from './RTCPeerConnection.js';
-import RTCSessionDescription from './RTCSessionDescription.js';
-import RTCIceCandidate from './RTCIceCandidate.js';
-import RTCDataChannel from './RTCDataChannel.js';
-import { RTCDataChannelEvent, RTCPeerConnectionIceEvent } from './Events.js';
+export { default as RTCCertificate } from './RTCCertificate.js';
+export { default as RTCDataChannel } from './RTCDataChannel.js';
+export { default as RTCDtlsTransport } from './RTCDtlsTransport.js';
+export { default as RTCIceCandidate } from './RTCIceCandidate.js';
+export { default as RTCIceTransport } from './RTCIceTransport.js';
+export { default as RTCPeerConnection } from './RTCPeerConnection.js';
+export { default as RTCSctpTransport } from './RTCSctpTransport.js';
+export { default as RTCSessionDescription } from './RTCSessionDescription.js';
+
+export { RTCDataChannelEvent, RTCPeerConnectionIceEvent } from './Events.js';
 
 export default {
-    RTCPeerConnection,
-    RTCSessionDescription,
-    RTCIceCandidate,
+    RTCCertificate,
     RTCDataChannel,
+    RTCDtlsTransport,
+    RTCIceCandidate,
+    RTCIceTransport,
+    RTCPeerConnection,
+    RTCSctpTransport,
+    RTCSessionDescription,
     RTCDataChannelEvent,
     RTCPeerConnectionIceEvent,
 };
