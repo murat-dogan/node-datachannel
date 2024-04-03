fb91e2eae05e0bd297ecad99ef73580ae8b40a07
Murat Dogan <doganmurat@gmail.com>
2024-04-03 19:16:40 +0200

    fix console logs

--- test/websockets.js
+++ test/websockets.js
@@ -6,19 +6,19 @@ nodeDataChannel.preload();
 const webSocketServer = new nodeDataChannel.WebSocketServer({ bindAddress: '127.0.0.1', port: 1987 });
 
 webSocketServer.onClient((serverSocket) => {
-    console.error('webSocketServer.onClient()');
+    console.log('webSocketServer.onClient()');
 
     serverSocket.onOpen(() => {
-        console.error('serverSocket.onOpen()');
+        console.log('serverSocket.onOpen()');
     });
 
     serverSocket.onMessage((message) => {
-        console.error('serverSocket.onMessage():', message);
-        serverSocket.sendMessage('reply to' + message);
+        console.log('serverSocket.onMessage():', message);
+        serverSocket.sendMessage('reply to ' + message);
     });
 
     serverSocket.onClosed(() => {
-        console.error('serverSocket.onClosed()');
+        console.log('serverSocket.onClosed()');
         serverSocket.close();
     });
 });
@@ -26,18 +26,18 @@ webSocketServer.onClient((serverSocket) => {
 const clientSocket = new nodeDataChannel.WebSocket();
 
 clientSocket.onOpen(() => {
-    console.error('clientSocket.onOpen()');
+    console.log('clientSocket.onOpen()');
     clientSocket.sendMessage('Hello');
 });
 
 clientSocket.onMessage((message) => {
-    console.error('clientSocket.onMessage():', message);
+    console.log('clientSocket.onMessage():', message);
     clientSocket.close();
     webSocketServer.stop();
 });
 
 clientSocket.onClosed(() => {
-    console.error('clientSocket.onClosed()');
+    console.log('clientSocket.onClosed()');
     clientSocket.close();
 });
 
