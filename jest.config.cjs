0abeeb2e034e17b9b4b17a4500e0251e06fddda9
Murat Dogan <doganmurat@gmail.com>
2024-04-15 18:52:37 +0200

    remove multiple run from def tests

---
+++ jest.config.cjs
@@ -0,0 +1,7 @@
+/** @type {import('jest').Config} */
+const config = {
+    verbose: true,
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', 'multiple-run.test'],
+};
+
+module.exports = config;
