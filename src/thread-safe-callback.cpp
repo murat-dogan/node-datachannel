428704f11c899134b5d4a74245a04cf5f060c4fb
Paul-Louis Ageneau <paul-louis@ageneau.org>
2024-05-06 10:49:35 +0200

    Remove problematic and useless ThreadSafeCallback context

--- src/thread-safe-callback.cpp
+++ src/thread-safe-callback.cpp
@@ -9,16 +9,16 @@ const char *ThreadSafeCallback::CancelException::what() const throw()
 
 ThreadSafeCallback::ThreadSafeCallback(Napi::Function callback)
 {
-    if (!callback.IsFunction())
-        throw Napi::Error::New(callback.Env(), "Callback must be a function");
-
     Napi::Env env = callback.Env();
 
-    receiver = Napi::Persistent(static_cast<Napi::Value>(Napi::Object::New(env)));
+    if (!callback.IsFunction())
+        throw Napi::Error::New(env, "Callback must be a function");
+
     tsfn = tsfn_t::New(env,
                        std::move(callback),
                        "ThreadSafeCallback callback",
-                       0, 1, &receiver);
+                       0, // unlimited queue
+                       1);
 }
 
 ThreadSafeCallback::~ThreadSafeCallback()
@@ -38,7 +38,7 @@ void ThreadSafeCallback::call(arg_func_t argFunc, cleanup_func_t cleanupFunc)
 
 void ThreadSafeCallback::callbackFunc(Napi::Env env,
                                       Napi::Function callback,
-                                      Napi::Reference<Napi::Value> *context,
+                                      ContextType *context,
                                       CallbackData *data)
 {
     // if env is gone this could mean cb fn has changed. See issue#176
@@ -61,7 +61,7 @@ void ThreadSafeCallback::callbackFunc(Napi::Env env,
 
     if (env && callback)
     {
-        callback.Call(context->Value(), args);
+        callback.Call(args);
         cleanup();
     }
 }
