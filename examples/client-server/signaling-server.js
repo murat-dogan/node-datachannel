0df4b4a9d8de22dfa3065d99ade53e16d6df0f42
Murat Dogan <doganmurat@gmail.com>
2024-07-06 13:01:46 +0200

    move path read to onOpen

--- examples/client-server/signaling-server.js
+++ examples/client-server/signaling-server.js
@@ -8,33 +8,34 @@ const clients = {};
 const wsServer = new nodeDataChannel.WebSocketServer({ bindAddress: '127.0.0.1', port: 8000 });
 
 wsServer.onClient((ws) => {
-    // path workaround
-    setTimeout(() => {
-        const id = ws.path().replace('/', '');
+    let id = '';
+
+    ws.onOpen(() => {
+        id = ws.path().replace('/', '');
         console.log(`New Connection from ${id}`);
-
         clients[id] = ws;
-        ws.onMessage((buffer) => {
-            let msg = JSON.parse(buffer);
-            let peerId = msg.id;
-            let peerWs = clients[peerId];
+    });
 
-            console.log(`Message from ${id} to ${peerId} : ${buffer}`);
-            if (!peerWs) return console.error(`Can not find peer with ID ${peerId}`);
+    ws.onMessage((buffer) => {
+        let msg = JSON.parse(buffer);
+        let peerId = msg.id;
+        let peerWs = clients[peerId];
 
-            msg.id = id;
-            peerWs.sendMessage(JSON.stringify(msg));
-        });
+        console.log(`Message from ${id} to ${peerId} : ${buffer}`);
+        if (!peerWs) return console.error(`Can not find peer with ID ${peerId}`);
 
-        ws.onClosed(() => {
-            console.log(`${id} disconnected`);
-            delete clients[id];
-        });
+        msg.id = id;
+        peerWs.sendMessage(JSON.stringify(msg));
+    });
 
-        ws.onError((err) => {
-            console.error(err);
-        });
-    }, 100);
+    ws.onClosed(() => {
+        console.log(`${id} disconnected`);
+        delete clients[id];
+    });
+
+    ws.onError((err) => {
+        console.error(err);
+    });
 });
 
 // There is a bug in the library that causes the WebSocketServer to be garbage collected
