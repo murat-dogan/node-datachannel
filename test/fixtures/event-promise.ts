1e84d6b053c2391fa464085fa0771269bfef91f6
achingbrain <alex@achingbrain.net>
2025-04-15 17:56:32 +0100

    chore: use opts

--- test/fixtures/event-promise.ts
+++ test/fixtures/event-promise.ts
@@ -2,13 +2,13 @@ export interface EventPromiseOptions {
   errorEvent?: string
 }
 
-export async function eventPromise <T = unknown> (emitter: EventTarget, event: string): Promise<T> {
+export async function eventPromise <T = unknown> (emitter: EventTarget, event: string, opts?: EventPromiseOptions): Promise<T> {
   return new Promise<T>((resolve, reject) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     emitter.addEventListener(event, (evt: any) => {
       resolve(evt);
     });
-    emitter.addEventListener('error', (err) => {
+    emitter.addEventListener(opts?.errorEvent ?? 'error', (err) => {
       reject(err);
     });
   });
