0ec58d203d72a3c7eb28368e6fe43858e22958e4
mertushka <mertushkaonline@gmail.com>
2025-06-19 04:36:47 +0300

    chore: remove unneeded closing check

--- src/polyfill/RTCDataChannel.ts
+++ src/polyfill/RTCDataChannel.ts
@@ -49,11 +49,6 @@ export default class RTCDataChannel extends EventTarget implements globalThis.RT
       // Simulate closing event
       if (this.#readyState === 'closed') return;
 
-      if (this.#readyState !== 'closing') {
-        this.#readyState = 'closing';
-        this.dispatchEvent(new Event('closing'));
-      }
-
       setImmediate(() => {
         if (this.#readyState !== 'closed') {
           this.#readyState = 'closed';
