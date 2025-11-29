c91ba4ccd295dc7d5ebe1a47f3ae8885a8a436d6
Long Nguyen <nguyen.long.908132@gmail.com>
2025-11-29 20:22:42 +0700

    Actually initialize it

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -5,7 +5,14 @@
 #include "ice-udp-mux-listener-wrapper.h"
 
 #if RTC_ENABLE_MEDIA == 1
+#include "media-av1rtppacketizer-wrapper.h"
+#include "media-h264rtppacketizer-wrapper.h"
+#include "media-h265rtppacketizer-wrapper.h"
+#include "media-rtcpnackresponder-wrapper.h"
 #include "media-rtcpreceivingsession-wrapper.h"
+#include "media-rtcpsrreporter-wrapper.h"
+#include "media-rtppacketizationconfig-wrapper.h"
+#include "media-rtppacketizer-wrapper.h"
 #include "media-track-wrapper.h"
 #include "media-video-wrapper.h"
 #include "media-audio-wrapper.h"
@@ -21,7 +28,14 @@ Napi::Object InitAll(Napi::Env env, Napi::Object exports)
   RtcWrapper::Init(env, exports);
 
 #if RTC_ENABLE_MEDIA == 1
+  AV1RtpPacketizerWrapper::Init(env, exports);
+  H264RtpPacketizerWrapper::Init(env, exports);
+  H265RtpPacketizerWrapper::Init(env, exports);
+  RtcpNackResponderWrapper::Init(env, exports);
   RtcpReceivingSessionWrapper::Init(env, exports);
+  RtcpSrReporterWrapper::Init(env, exports);
+  RtpPacketizationConfigWrapper::Init(env, exports);
+  RtpPacketizerWrapper::Init(env, exports);
   TrackWrapper::Init(env, exports);
   VideoWrapper::Init(env, exports);
   AudioWrapper::Init(env, exports);
