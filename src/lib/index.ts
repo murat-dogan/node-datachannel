c43cfed1bb7513da83bb47aacc249c2f77f0e7ac
Long Nguyen <nguyen.long.908132@gmail.com>
2025-12-28 14:00:34 +0700

    Types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -202,6 +202,12 @@ export interface MediaHandler {
   addToChain(handler: MediaHandler): void
 }
 
+export interface PacingHandler extends MediaHandler {}
+
+export const PacingHandler: {
+  new (bitsPerSecond: number, sendInterval: number): PacingHandler;
+} = nodeDataChannel.PacingHandler;
+
 export interface RtcpReceivingSession extends MediaHandler {}
 
 export const RtcpReceivingSession: {
